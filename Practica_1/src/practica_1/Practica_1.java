171daa5a397d2b6188467686b2209a21459db40a
Miguel galicia <3776080760101@ingenieria.usac.edu.gt>
2023-02-04 16:45:07 -0600

    Parche 1.5
    Opciop descuento

--- Practica_1/src/practica_1/Practica_1.java
+++ Practica_1/src/practica_1/Practica_1.java
@@ -315,7 +315,7 @@ public class Practica_1 {
         }
         System.out.println("Nombre: "+nombre);
         System.out.println("NIT: "+nit);
-        System.out.println("Hay codigo de descuento?");
+        System.out.println("Hay codigo de descuento, si o no?");
         String respuesta= sc.nextLine();
         if(respuesta.equals("si")){
             System.out.println("Ingrese el codigo");
